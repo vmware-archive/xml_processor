3b20b153984c9af111a8938666f8ad66d91f2387
Frank Kotsianas <frankkotsianas@gmail.com>
2015-03-27 15:24:34 -0700

    use dummy name and email for git config
    Signed-off-by: Elena Sharma <esharma@pivotal.io>

--- lib/github_client.rb
+++ lib/github_client.rb
@@ -15,7 +15,7 @@ class GithubClient
   def push(directory)
     FileUtils.chdir(directory) do
         g = Git.open(directory)
-        g.config('user.name', 'name')
+        g.config('user.name', 'pipeline')
         g.config('user.email', 'email@example.com')
         begin
         g.add
