dbe39c153014ae2fd624519a71fa896b2b42d649
Eric Tsiliacos <etsiliacos@pivotal.io>
2015-03-30 15:22:06 -0700

    only push contents in workspace

--- push.rb
+++ push.rb
@@ -7,7 +7,7 @@ ARGV.each do |arg|
   github_client = GithubClient.new
   github_client.add_listener(view_updater)
   Dir.mktmpdir do |tmpdir|
-    Pusher.new(github_client, Pathname.new(tmpdir)).run(arg, Pathname('output'))
+    Pusher.new(github_client, Pathname.new(tmpdir)).run(arg, Pathname('output/workspace'))
   end
 end
 
