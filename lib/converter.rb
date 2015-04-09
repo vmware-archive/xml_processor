56e3960833cc889764543204f4a014b8b944ead3
Andrew Bruce <andrew@camelpunch.com>
2015-04-09 15:05:27 +0100

    Break Converter apart a bit
    [#91521520]

--- lib/converter.rb
+++ lib/converter.rb
@@ -8,43 +8,83 @@ require_relative 'write_in_directory'
 require_relative 'xslt_processor'
 
 class Converter
-  SUPPORTED_EXTS = %w[jpg jpeg png tif tiff emf]
-
   def initialize(output_dir)
     @output_dir = output_dir
   end
 
   def run(dirs)
     dirs.each do |dir|
-      FileUtils.remove_dir("#{@output_dir}/#{dir}", true)
+      DirProcessor.new(dir, output_dir).call
+    end
+  end
 
-      files = Dir.glob("#{dir}/**/*")
-      xml_files = files.select { |f| is_xml_file?(f) }
-      other_files = files.select { |f| is_non_xml_file?(f) }
+  private
 
-      xyleme_files = xml_files.reduce({}) do |hash, filepath|
-        file_contents = File.read(filepath)
-        hash.merge(filepath => file_contents)
-      end
+  def output_dir
+    Pathname(@output_dir)
+  end
+end
 
-      processes = [
-          XsltProcessor.new(File.read('xyleme_to_html.xsl')),
-          ReplaceWordsInText.new('Hortonworks' => 'Pivotal'),
-          AddFileExtentions.new(%w[erb]),
-          AddFrontmatter.new('Pivotal Hadoop Documentation'),
-          WriteInDirectory.new(@output_dir)
-      ]
+class DirProcessor
+  UNDERSCORE_WHITESPACE_EXTENSIONS = %w[.jpg .jpeg .png .tif .tiff .emf]
 
-      processed_output = processes.reduce(xyleme_files) { |arg, process| process.call(arg) }
+  def initialize(dir, output_dir)
+    @dir = dir
+    @output_dir = output_dir
+  end
 
-      other_files.each do |file|
-        file_extension = File.extname(file).gsub('.', '')
-        formatted_file_path = SUPPORTED_EXTS.include?(file_extension) ? file.gsub(' ', '_') : file
+  def call
+    FileUtils.remove_dir("#{output_dir}/#{dir}", true)
+    processes(output_dir).reduce(transformable_files) { |arg, process| process.call(arg) }
+    copy_to_formatted_paths(other_files)
+  end
 
-        dest_path = Pathname("#{@output_dir}/#{formatted_file_path}")
+  private
+
+  attr_reader :dir, :files, :xml_files, :output_dir
+
+  def processes(output_dir)
+    [
+      XsltProcessor.new(File.read('xyleme_to_html.xsl')),
+      ReplaceWordsInText.new('Hortonworks' => 'Pivotal'),
+      AddFileExtentions.new(%w[erb]),
+      AddFrontmatter.new('Pivotal Hadoop Documentation'),
+      WriteInDirectory.new(output_dir)
+    ]
+  end
+
+  def copy_to_formatted_paths(src_paths)
+    src_paths.each do |src_path|
+      output_dir.join(format_file_path(src_path)).tap do |dest_path|
         dest_path.dirname.mkpath
-        FileUtils.copy(file, dest_path)
+        FileUtils.copy(src_path, dest_path)
       end
     end
+  end
+
+  def format_file_path(path)
+    if UNDERSCORE_WHITESPACE_EXTENSIONS.include?(path.extname)
+      path.to_s.gsub(' ', '_')
+    else
+      path
+    end
+  end
+
+  def transformable_files
+    xml_files.reduce({}) do |hash, filepath|
+      hash.merge(filepath => File.read(filepath))
+    end
+  end
+
+  def xml_files
+    @xml_files ||= files.select { |f| is_xml_file?(f) }
+  end
+
+  def other_files
+    @other_files ||= files.select { |f| is_non_xml_file?(f) }.map { |f| Pathname(f) }
+  end
+
+  def files
+    @files ||= Dir.glob("#{dir}/**/*")
   end
 end
