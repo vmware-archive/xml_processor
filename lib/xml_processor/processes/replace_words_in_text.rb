6afda219e764fe0ea11f635c21c797e0b6dfd572
Andrew Bruce <andrew@camelpunch.com>
2015-04-09 16:11:18 +0100

    More modulizing
    [#91521520]

--- lib/xml_processor/processes/replace_words_in_text.rb
+++ lib/xml_processor/processes/replace_words_in_text.rb
@@ -1,13 +1,16 @@
-class ReplaceWordsInText
-  def initialize(replace_pairs)
-    @replace_pairs = replace_pairs
-  end
+module XmlProcessor
+  module Processes
+    class ReplaceWordsInText
+      def initialize(replace_pairs)
+        @replace_pairs = replace_pairs
+      end
 
-  def call(files)
-    files.reduce({}) do |output, (file_path, text)|
-      transformed_data = @replace_pairs.reduce(text) { |str, (k,v)| str.gsub(k,v) }
-      output.merge({file_path => transformed_data})
+      def call(files)
+        files.reduce({}) do |output, (file_path, text)|
+          transformed_data = @replace_pairs.reduce(text) { |str, (k,v)| str.gsub(k,v) }
+          output.merge({file_path => transformed_data})
+        end
+      end
     end
   end
-
 end
--- lib/xml_processor/processes/write_in_directory.rb
+++ lib/xml_processor/processes/write_in_directory.rb
@@ -1,15 +1,19 @@
-class WriteInDirectory
-  def initialize(output_dir)
-    @output_dir = output_dir
-  end
+module XmlProcessor
+  module Processes
+    class WriteInDirectory
+      def initialize(output_dir)
+        @output_dir = output_dir
+      end
 
-  def call(files)
-    FileUtils.mkdir_p(@output_dir)
+      def call(files)
+        FileUtils.mkdir_p(@output_dir)
 
-    files.each do |filepath, file_content|
-      base_path = Pathname(filepath).dirname
-      FileUtils.mkdir_p("#{@output_dir}/#{base_path}")
-      File.open("#{@output_dir}/#{filepath}", 'w+') { |file| file.write(file_content) }
+        files.each do |filepath, file_content|
+          base_path = Pathname(filepath).dirname
+          FileUtils.mkdir_p("#{@output_dir}/#{base_path}")
+          File.open("#{@output_dir}/#{filepath}", 'w+') { |file| file.write(file_content) }
+        end
+      end
     end
   end
 end
