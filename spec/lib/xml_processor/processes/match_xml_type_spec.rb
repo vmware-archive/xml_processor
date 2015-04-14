a020bba8ab515251a53e42bb93007791e4c60e0f
Andrew Bruce <andrew@camelpunch.com>
2015-04-14 15:46:55 +0100

    Match root nodes regardless of namespace
    [#91521520]

--- spec/lib/xml_processor/processes/match_xml_type_spec.rb
+++ spec/lib/xml_processor/processes/match_xml_type_spec.rb
@@ -3,15 +3,17 @@ require_relative '../../../../lib/xml_processor/processes/match_xml_type'
 module XmlProcessor
   module Processes
     describe MatchXmlType do
-      it 'selects files that contain the desired XML identifier and returns a hash' do
+      it 'only permits documents containing the specified root node' do
         files = {
-            'file.xml' => "<RootNode><OtherNode></OtherNode></RootNode>",
-            'file_2.xml' => "<WrongRootNode><SomeRandomNode></SomeRandomNode></WrongRootNode>"
+          'file.xml' => '<RootNode xmlns="http://my.namespace/"><OtherNode></OtherNode></RootNode>',
+          'file_2.xml' => "<WrongRootNode><SomeRandomNode></SomeRandomNode></WrongRootNode>"
         }
 
         matcher = MatchXmlType.new(:RootNode)
-        expect(matcher.call files).to eq({ 'file.xml' => "<RootNode><OtherNode></OtherNode></RootNode>" })
+        expect(matcher.call(files)).
+          to eq('file.xml' => '<RootNode xmlns="http://my.namespace/"><OtherNode></OtherNode></RootNode>')
       end
     end
   end
 end
+
