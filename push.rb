2bbcfb0abde3df17f0304a9c2f533183f3f375b5
Frank Kotsianas <frankkotsianas@gmail.com>
2015-04-16 09:11:00 -0700

    GithubClient renamed to GitClient in push.rb.
    [#91521520]
    
    Signed-off-by: Elena Sharma <esharma@pivotal.io>

--- push.rb
+++ push.rb
@@ -6,7 +6,7 @@ require_relative 'lib/view_updater'
 
 ARGV.each do |arg|
   view_updater = ViewUpdater.new
-  github_client = GithubClient.new
+  github_client = GitClient.new
   github_client.add_listener(view_updater)
   Dir.mktmpdir do |tmpdir|
     Pusher.new(github_client, Pathname.new(tmpdir)).run(arg, Pathname('output/workspace'))
